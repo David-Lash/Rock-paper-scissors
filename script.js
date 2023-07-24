220b510f059de47edd840c0b24bfbeaead2c3f19
David-Lash <96846566+David-Lash@users.noreply.github.com>
2023-07-24 10:26:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function getComputerChoice(){
-  return rock || paper || scissors;
+  return "rock" || "paper" || "scissors";
 }
 
 console.log(getComputerChoice);
+
+function playRound(playerSelection, computerSelection){
+playerSelection = "rock";
+computerSelection = getComputerChoice();
+return "You Lose! Paper beats Rock";
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));    
+}
+
+const obj= window.prompt("Enter rock, paper or scissors", "");
